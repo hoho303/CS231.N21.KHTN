1cf13fa12e076198ae0490ed3d80cd1721f5a0b7
hoho303 <hoangthuan2092003@gmail.com>
2023-06-05 14:46:02 +0700

    update path

--- Demo/constants.py
+++ Demo/constants.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_DIR = "Dataset"
+DATA_DIR = "dataset"
 INDEX_LOOKUP_FILE = os.path.join(DATA_DIR, "image_index.csv")
 INDEX_DIR = os.path.join(DATA_DIR, "image_indexes")
 IMAGE_DIR = os.path.join(DATA_DIR, "images")
